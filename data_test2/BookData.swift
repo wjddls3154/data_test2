a3a37eb2e9631c7f5a42ac6494bb87e34f4674cf
k <k@ks-Mac.local>
2018-10-24 09:13:41 -0700

    데이터클래스 1.book.xml생성 내용삽입 2.BookData생성 내용채워놓기 3.메인스토리보드수정 4.뷰컨트롤러수정 선택5(외부공공데이터xml파싱시 info부분에 transfer 옵션 설정해줘야함)

---
+++ data_test2/BookData.swift
@@ -0,0 +1,13 @@
+//
+//  BookData.swift
+//  data_test2
+//
+//  Created by k on 헤이세이 30. 10. 24..
+//  Copyright © 헤이세이 30년 ksh. All rights reserved.
+//
+
+import Foundation
+class BookData {
+    var title = ""
+    var author = ""
+}
--- data_test2/ViewController.swift
+++ data_test2/ViewController.swift
@@ -8,18 +8,89 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, XMLParserDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var myTableView: UITableView!
+    var myBookData = [BookData]()
+    var BTitle = ""
+    var BAuthor = ""
+    var currentElement = ""
+    // 배열 선언
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //xml 파일 소문자 대문자 확실히 인지.
+        if let path = Bundle.main.url(forResource: "book", withExtension: "xml") {
+            if let myParser = XMLParser(contentsOf: path) {
+                myParser.delegate = self
+                myTableView.delegate = self
+                myTableView.dataSource = self
+                if myParser.parse() {
+                    print("Parsing succeed")
+                    for i in 0 ..< myBookData.count {
+                        print(myBookData[i].title)
+                        print(myBookData[i].author)
+                    }
+                } else {
+                    print("Parsing failed")
+                }
+            } else {
+                print("path failed")
+            }
+        } else {
+            print("file error")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+        print(currentElement)
     }
-
-
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        //공백 제거하기
+        let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        //빈칸 아니면 출력
+        if !data.isEmpty {
+            switch currentElement {
+            case "title" : BTitle = data
+            case "author" : BAuthor = data
+            default : break
+            }
+        }
+    }
+    
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "book" {
+            let myItem = BookData()
+            myItem.title = BTitle
+            myItem.author = BAuthor
+            myBookData.append(myItem)
+            
+            //myitem에 내용 추가
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myBookData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = myTableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        myCell.textLabel?.text = myBookData[indexPath.row].title
+        myCell.detailTextLabel?.text = myBookData[indexPath.row].author
+        
+        //서브타이틀을 이용한 데이터 출력
+        
+        
+        
+        //태그를 이용한 데이터 출력
+        //        let CTitle = myCell.viewWithTag(1) as! UILabel
+        //        let CAuthor = myCell.viewWithTag(2) as! UILabel
+        //        CTitle.text = myBookData[indexPath.row].title
+        //        CAuthor.text = myBookData[indexPath.row].author
+        
+        return myCell
+    }
+    
 }
-
